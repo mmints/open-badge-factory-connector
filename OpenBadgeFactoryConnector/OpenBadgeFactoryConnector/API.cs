4f48e782b571e895e6e51efdbc8a2e60e819baaa
Mark Oliver Mints <mmints@uni-koblenz.de>
2019-02-13 17:46:26 +0100

    [Update] Remove HttpClient because we're going to use unity's functions

--- OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/API.cs
+++ OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/API.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Net.Http;
 
 namespace OpenBadgeFactoryConnector
 {
@@ -8,14 +7,12 @@ namespace OpenBadgeFactoryConnector
         private string url;
         private string certificate_file;
         private string key_file;
-        private HttpClient client;
 
         public API(string url, string certificate_file, string key_file)
         {
             this.url = url;
             this.certificate_file = certificate_file;
             this.key_file = key_file;
-            this.client = new HttpClient();
         }
     }
 }
