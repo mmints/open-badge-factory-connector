411f08cf2676508b50e487a07b32dce0cbc3f6ce
Mark Oliver Mints <mmints@uni-koblenz.de>
2019-02-13 16:38:10 +0100

    [Init] Add main components of the api connector

---
+++ OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/API.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace OpenBadgeFactoryConnector
+{
+    public class API
+    {
+        private string url;
+        private string certificate_file;
+        private string key_file;
+    
+        public API(string url, string certificate_file, string key_file)
+        {
+            this.url = url;
+            this.certificate_file = certificate_file;
+            this.key_file = key_file;
+        }
+    }
+}
---
+++ OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/Connector.cs
@@ -0,0 +1,14 @@
+using System.Net.WebSockets;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OpenBadgeFactoryConnector
+{
+    public class Connector
+    {
+        public Connector(string client_id, string certificate_file, string key_file)
+        {
+            
+        }
+    }
+}
