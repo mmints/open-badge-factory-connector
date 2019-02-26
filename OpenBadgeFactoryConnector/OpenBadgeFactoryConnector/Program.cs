70c842880806436fbe7aae3d6ff325763e6ed9a4
Mark Mints <mmints@uni-koblenz.de>
2019-02-26 23:40:42 +0100

    [Update] Successful loading certificate

--- OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/Program.cs
+++ OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace OpenBadgeFactoryConnector
@@ -8,26 +9,43 @@ namespace OpenBadgeFactoryConnector
     {        
         static async Task Main()
         {
-            // Create a New HttpClient object and dispose it when done, so the app doesn't leak resources
-            using (HttpClient client = new HttpClient())
-            {
-                // Call asynchronous network methods in a try/catch block to handle exceptions
-                try	
-                {
-                    HttpResponseMessage response = await client.GetAsync("https://openbadgefactory.com/v1/ping/NM70OHe7HCeO");
-                    // response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    // Above three lines can be replaced with new helper method below
-                    // string responseBody = await client.GetStringAsync(uri);
+            
+            // The path to the certificate.
+            string Certificate = "/Users/mark/Developer/open-badge-factory-connector-cs/OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/certificates/certificate.pem";
+            
+            // Load the certificate into an X509Certificate object.
+            X509Certificate cert = new X509Certificate(Certificate);
+            
+            string resultsTrue = cert.ToString(true);
+            Console.WriteLine(resultsTrue);
+            
+            string resultsFalse = cert.ToString(false);
+            Console.WriteLine(resultsFalse);
+            
+            // Create an HttpClientHandler object and set to use default credentials
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
 
-                    Console.WriteLine(responseBody);
-                }  
-                catch(HttpRequestException e)
-                {
-                    Console.WriteLine("\nException Caught!");	
-                    Console.WriteLine("Message :{0} ",e.Message);
-                }
+            // Create an HttpClient object
+            HttpClient client = new HttpClient(handler);
+            
+            // Call asynchronous network methods in a try/catch block to handle exceptions
+            try	
+            {
+                string responseBody = await client.GetStringAsync("https://openbadgefactory.com/v1/ping/NM70OHe7HCeO");
+
+                Console.WriteLine(responseBody);
+            }  
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");	
+                Console.WriteLine("Message :{0} ",e.Message);
             }
+
+            // Need to call dispose on the HttpClient and HttpClientHandler objects 
+            // when done using them, so the app doesn't leak resources
+            handler.Dispose();
+            client.Dispose();
         }
     }
 }
