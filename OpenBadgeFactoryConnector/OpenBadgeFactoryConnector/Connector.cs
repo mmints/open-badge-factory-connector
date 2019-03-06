f352e1b83c26c10f50c4a913fbc77e735a8b3098
Mark Oliver Mints <mmints@uni-koblenz.de>
2019-03-06 16:33:04 +0100

    [Update] WIP: Implement connector to obf

--- OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/Connector.cs
+++ OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/Connector.cs
@@ -1,15 +1,39 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
 
 namespace OpenBadgeFactoryConnector
 {
+    /// <summary>
+    /// Class<c>Connector</c>
+    /// Top level communication connector to open badge factory's API.
+    /// </summary>
     public class Connector
     {
         private string client_id;
         private API api;
-        public Connector(string client_id, string certificate_file)
+        private X509Certificate2 certificate; 
+        
+        /// <summary>
+        /// Constructor <c>Connector</c>
+        /// </summary>
+        /// <param name="client_id">Client ID of the requester</param>
+        public Connector(string client_id)
         {
+            this.certificate = new X509Certificate2("C:\\Users\\Mark\\IWM\\obf-certificates\\cert.pfx", "odl4u"); // Just for testing 
             this.client_id = client_id;
-            this.api = new API("https://openbadgefactory.com/v1", certificate_file); // The entered URL is given by open badge factory
+            this.api = new API("https://openbadgefactory.com/v1", this.certificate, this.client_id);
+        }
+
+        /// <summary>
+        /// This function returns all accessible badges.
+        /// </summary>
+        /// <returns>response bode (json) which includes all badges.</returns>
+        public async Task<string> GetAllBadges()
+        {
+            string responseBody = await this.api.GetRequest("badge");
+            return responseBody;
         }
     }
 }
