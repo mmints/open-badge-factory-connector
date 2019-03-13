bc0a4966807d98c8c72ab8d03c5fb49de844838f
Mark Oliver Mints <mmints@uni-koblenz.de>
2019-03-13 16:01:00 +0100

    [Refactor] Clean up Connector

--- OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/Connector.cs
+++ OpenBadgeFactoryConnector/OpenBadgeFactoryConnector/Connector.cs
@@ -11,19 +11,16 @@ namespace OpenBadgeFactoryConnector
     /// </summary>
     public class Connector
     {
-        private string client_id;
-        private API api;
-        private X509Certificate2 certificate; 
-        
+        private readonly API _api;
+
         /// <summary>
         /// Constructor <c>Connector</c>
         /// </summary>
-        /// <param name="client_id">Client ID of the requester</param>
-        public Connector(string client_id)
+        /// <param name="clientId">Client ID of the requester</param>
+        public Connector(string clientId)
         {
-            this.certificate = new X509Certificate2("C:\\Users\\Mark\\IWM\\obf-certificates\\cert.pfx", "odl4u"); // Just for testing 
-            this.client_id = client_id;
-            this.api = new API("https://openbadgefactory.com/v1", this.certificate, this.client_id);
+            X509Certificate2 certificate = new X509Certificate2("certificates/cert.pfx", "odl4u"); // Just for testing
+            this._api = new API("https://openbadgefactory.com/v1", certificate, clientId);
         }
 
         /// <summary>
@@ -32,7 +29,7 @@ namespace OpenBadgeFactoryConnector
         /// <returns>response bode (json) which includes all badges.</returns>
         public async Task<string> GetAllBadges()
         {
-            string responseBody = await this.api.GetRequest("badge");
+            var responseBody = await this._api.GetRequest("badge");
             return responseBody;
         }
     }
